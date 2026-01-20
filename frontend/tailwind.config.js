13fc282c35323eba4b3669b0f198fc6c8815d33f
el Atoubi Charif.Mohamed <mohamed.atoubi.externo@salud.madrid.org>
2026-01-20 16:21:49 +0100

    Primer commit

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: [
+    "../frontend/pages/**/*.{js,ts,jsx,tsx}",
+    "../frontend/components/**/*.{js,ts,jsx,tsx}",
     "./frontend/pages/**/*.{js,ts,jsx,tsx}",
-    "./frontend/components/**/*.{js,ts,jsx,tsx}"
+    "./frontend/components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
